025506a9f04a60a407957c0589383dd476bad2d8
Florian Bouvot <florian.bouvot@gmail.com>
2018-03-28 10:53:29 +0200

    Adapt project to tweeb needs

--- README.md
+++ README.md
@@ -6,14 +6,8 @@ https://www.tinymce.com/download/language-packages/
 
 ## Instalation
 
-Via npm
-
-```bash
-$ npm install tinymce-i18n
-```
-
 Via composer
 
-```bash
-$ composer require mklkj/tinymce-i18n dev-master
+```
+$ composer require tweeb/tinymce-i18n
 ```
--- update.sh
+++ update.sh
@@ -1,65 +1,7 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-wget \
-	"https://tinymce-services.azurewebsites.net/1/i18n/download?langs="`
-	`"af_ZA,"`
-	`"ar,"`
-	`"be,"`
-	`"bg_BG,"`
-	`"ca,"`
-	`"zh_CN,"`
-	`"zh_TW,"`
-	`"hr,"`
-	`"cs,"`
-	`"da,"`
-	`"dv,"`
-	`"nl,"`
-	`"en_CA,"`
-	`"en_GB,"`
-	`"et,"`
-	`"fr_FR,"`
-	`"gl,"`
-	`"ka_GE,"`
-	`"de,"`
-	`"de_AT,"`
-	`"el,"`
-	`"he_IL,"`
-	`"hu_HU,"`
-	`"ga,"`
-	`"it,"`
-	`"ja,"`
-	`"kab,"`
-	`"kk,"`
-	`"km_KH,"`
-	`"ko_KR,"`
-	`"lv,"`
-	`"nb_NO,"`
-	`"fa_IR,"`
-	`"pl,"`
-	`"pt_BR,"`
-	`"pt_PT,"`
-	`"ro,"`
-	`"ru,"`
-	`"sk,"`
-	`"sl_SI,"`
-	`"es,"`
-	`"es_MX,"`
-	`"sv_SE,"`
-	`"ta,"`
-	`"ta_IN,"`
-	`"th_TH,"`
-	`"tr,"`
-	`"tr_TR,"`
-	`"uk,"`
-	`"uk_UA,"`
-	`"uz,"`
-	`"vi_VN,"`
-	`"cy" \
-	-O langs.zip
+curl -o langs.zip https://tinymce-services.azurewebsites.net/1/i18n/download?langs=af_ZA,ar,eu,be,bg_BG,ca,zh_CN,zh_TW,hr,cs,da,dv,nl,en_CA,en_GB,et,fr_FR,gl,ka_GE,de,de_AT,el,he_IL,hu_HU,ga,it,ja,kab,kk,km_KH,ko_KR,lv,nb_NO,fa_IR,pl,pt_BR,pt_PT,ro,ru,sk,sl_SI,es,es_MX,sv_SE,ta,ta_IN,th_TH,tr,tr_TR,uk,uk_UA,uz,vi_VN,cy
 
 rm -r ./langs/
 
 unzip langs.zip
-
-jq ".version = \"`date +%y.%m.%d`\"" package.json > tmp.$$.json && mv tmp.$$.json package.json
-jq ".version = \"`date +%y.%m.%d`\"" composer.json > tmp.$$.json && mv tmp.$$.json composer.json
